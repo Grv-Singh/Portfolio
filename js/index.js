4588ddb058c93a6fc04d2106b5ef66ddddb590f8
Gaurav Singh <acad.grv97@gmail.com>
2020-11-21 11:18:19 +0530

    js external

---
+++ js/index.js
@@ -0,0 +1,89 @@
+// IIFE to ensure safe use of $
+(function( $ ) {
+
+  // Create plugin
+  $.fn.tooltips = function(el) {
+
+    var $tooltip,
+      $body = $('body'),
+      $el;
+
+    // Ensure chaining works
+    return this.each(function(i, el) {
+    
+      $el = $(el).attr("data-tooltip", i);
+
+      // Make DIV and append to page 
+      var $tooltip = $('<div class="tooltip" data-tooltip="' + i + '">' + $el.attr('title') + '<div class="arrow"></div></div>').appendTo("body");
+
+      // Position right away, so first appearance is smooth
+      var linkPosition = $el.position();
+
+      $tooltip.css({
+        top: linkPosition.top - $tooltip.outerHeight() - 13,
+        left: linkPosition.left - ($tooltip.width()/2)
+      });
+
+      $el
+      // Get rid of yellow box popup
+      .removeAttr("alt")
+
+      // Mouseenter
+      .hover(function() {
+
+        $el = $(this);
+
+        $tooltip = $('div[data-tooltip=' + $el.data('tooltip') + ']');
+
+        // Reposition tooltip, in case of page movement e.g. screen resize                        
+        var linkPosition = $el.position();
+
+        $tooltip.css({
+          top: linkPosition.top - $tooltip.outerHeight() - 13,
+          left: linkPosition.left - ($tooltip.width()/2)
+        });
+
+        // Adding class handles animation through CSS
+        $tooltip.addClass("active");
+
+        // Mouseleave
+      }, function() {
+
+        $el = $(this);
+
+        // Temporary class for same-direction fadeout
+        $tooltip = $('div[data-tooltip=' + $el.data('tooltip') + ']').addClass("out");
+
+        // Remove all classes
+        setTimeout(function() {
+          $tooltip.removeClass("active").removeClass("out");
+          }, 300);
+
+        });
+
+      });
+
+    }
+
+})(jQuery);
+
+$("img[title]").tooltips();
+
+"use strict";
+            function changeColor() {
+    //rgb
+    console.log('event fired');
+    var colors = [0, 0, 0];
+    var hour = new Date().getHours();
+    console.log(hour);
+
+    //Will get an valid rgb color
+    var color = parseInt(255/24*hour);
+    console.log(color);
+    for(var i = 0; i < colors.length; i++) {
+        colors[i] = color;
+    }
+    //add the color to the element you want:
+    $('div.parallax').css("background-color","rgb("+colors[0] + "," + colors[1] + "," + colors[2] + ")");
+    console.log("event fired. color get:" + colors[0] + colors[1] + colors[2]);
+  }
