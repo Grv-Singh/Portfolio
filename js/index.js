f50a991c58ad56a784cebb432165dfdb24981df0
Gaurav Singh <acad.grv97@gmail.com>
2020-11-21 11:24:56 +0530

    add

--- js/index.js
+++ js/index.js
@@ -70,16 +70,21 @@
 $("img[title]").tooltips();
 
 function changeColor() {
+    //rgb
+    console.log('event fired');
     var colors = [0, 0, 0];
     var hour = new Date().getHours();
+    console.log(hour);
 
     //Will get an valid rgb color
     var color = parseInt(255/24*hour);
+    console.log(color);
     for(var i = 0; i < colors.length; i++) {
         colors[i] = color;
     }
     //add the color to the element you want:
     $('div.parallax').css("background-color","rgb("+colors[0] + "," + colors[1] + "," + colors[2] + ")");
+    console.log("event fired. color get:" + colors[0] + colors[1] + colors[2]);
   }
 
 changeColor();
