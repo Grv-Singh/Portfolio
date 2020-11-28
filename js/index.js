0e06af6ffae338333641d371c45b0ddbf7f0cc0a
Gaurav Singh <12825441+Grv-Singh@users.noreply.github.com>
2020-11-28 12:39:03 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -91,3 +91,35 @@ changeColor();
 
 $("g.titles").innerHTML = '';
 
+Particles.
+init
+    ({
+
+        // normal options
+        selector: '.background',
+        maxParticles: 450,
+
+        // options for breakpoints
+        responsive: [{
+            breakpoint: 768,
+            options: {
+                maxParticles: 200,
+                color: '#48F2E3',
+                connectParticles: false
+            }
+        }, {
+            breakpoint: 425,
+            options: {
+                maxParticles: 100,
+                connectParticles: true
+            }
+        }, {
+            breakpoint: 320,
+            options: {
+                maxParticles: 0
+
+                // disables particles.js
+            }
+        }]
+    });
+
